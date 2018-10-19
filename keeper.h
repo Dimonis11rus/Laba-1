64293a4ed975243cee81ed26a4d5d53b2f73d241
Dimonis11rus <43698838+Dimonis11rus@users.noreply.github.com>
2018-10-19 19:11:09 +0300

    Update keeper.h

--- keeper.h
+++ keeper.h
@@ -1,2 +1,14 @@
-#include "keeper.cpp"
-class keeper;
+#ifndef KEEPER_H
+#define KEEPER_H
+
+class keeper
+{
+public:
+	void save_car(car &obj);
+    void save_motorbike(motorbike &obj);
+    void save_bus(bus &obj);
+    void read_file(car &obj_c, motorbike &obj_m, bus &obj_b, int &ii, int &car_count, int &moto_count, int &bus_count, char *str);
+
+};
+
+#endif
