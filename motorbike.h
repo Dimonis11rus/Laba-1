cd716ea2c950a2b85cc919d9f069697fc8e1149b
Dimonis11rus <43698838+Dimonis11rus@users.noreply.github.com>
2018-10-19 19:11:45 +0300

    Update motorbike.h

--- motorbike.h
+++ motorbike.h
@@ -1,2 +1,31 @@
-#include "motorbike.cpp"
-class motorbike;
+#ifndef MOTORBIKE_H
+#define MOTORBIKE_H
+
+class keeper;
+class motorbike
+{
+private:
+    char *motorbike_mark;
+    char *motorbike_model;
+    char *motorbike_engine;
+    char *motorbike_power;
+    char *motorbike_terrain;
+public:
+    friend keeper;
+	motorbike();
+	motorbike(char *a, char *b, char *c, char *d, char *e);
+	motorbike(const motorbike & arg);
+    void set_mark();         ///люпйю лнрнжхйкю
+    void get_mark();
+    void set_model();         ///лндекэ лнрнжхйкю
+    void get_model();
+    void set_moto_engine();         ///назел дбхфйю лнрнжхйкю
+    void get_moto_engine();
+    void set_power();         ///лнымнярэ лнрнжхйкю
+    void get_power();
+    void set_terrain();         ///леярмнярэ дкъ лнрнжхйкю
+    void get_terrain();
+    ~motorbike();
+};
+
+#endif
