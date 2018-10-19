6d06268791b6724c5bbb634f8f10c89fea82f00e
Dimonis11rus <43698838+Dimonis11rus@users.noreply.github.com>
2018-10-19 19:09:53 +0300

    Update car.h

--- car.h
+++ car.h
@@ -1,2 +1,31 @@
-#include "car.cpp"
-class car;
+#ifndef CAR_H
+#define CAR_H
+
+class keeper;
+class car : public garage
+{
+private:
+    char *car_mark;
+    char *car_model;
+    char *car_engine;
+    char *car_color;
+    char *car_kpp;
+public:
+    friend keeper;
+	car();
+	car(char *a, char *b, char *c, char *d, char *e);
+	car(const car & arg);
+    void set_mark();         ///люпйю юбрнлнахкъ
+    void get_mark();
+    void set_model();        ///лндекэ юбрнлнахкъ
+    void get_model();
+    void set_car_engine();         ///назел дбхфйю юбрнлнахкъ
+    void get_car_engine();
+    void set_color();         ///жбер юбрнлнахкъ
+    void get_color();
+    void set_kpp();         ///йнпнайю оепедюв юбрнлнахкъ
+    void get_kpp();
+    ~car();
+};
+
+#endif
