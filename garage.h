5503c813e86f662908cd68679e35129a79884bd8
Dimonis11rus <43698838+Dimonis11rus@users.noreply.github.com>
2018-10-19 19:10:05 +0300

    Update garage.h

--- garage.h
+++ garage.h
@@ -1,2 +1,15 @@
-#include "garage.cpp"
-class garage;
+#include "head.h"
+#ifndef GARAGE_H
+#define GARAGE_H
+
+class garage
+{
+public:
+    garage() {}
+    virtual void set_mark()=0;       ///Установка марки транспорта
+    virtual void get_mark()=0;       ///Получение марки транспорта
+    virtual void set_model()=0;      ///Установка модели транспорта
+    virtual void get_model()=0;      ///Получение модели транспорта
+    virtual ~garage() {cout<<"Гараж удалён!\n";}
+};
+#endif
